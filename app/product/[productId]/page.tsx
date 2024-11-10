199c67d2b961af8c624dad2e0487c221d09ed2fb
ZafarNorkulov <norqulovzafar123@gmail.com>
2024-11-10 17:36:21 +0500

    change

--- app/product/[productId]/page.tsx
+++ app/product/[productId]/page.tsx
@@ -3,26 +3,11 @@ import "./card.css";
 import ContactForm from "@/components/ContactForm";
 import PageClient from "./page.client";
 
-export async function generateStaticParams() {
-  try {
-    const res = await fetch(process.env.NEXT_PUBLIC_API + "product/");
-    if (!res.ok) {
-      throw new Error("Failed to fetch products");
-    }
 
-    const data = await res.json();
-
-    const products = data.results;
-    return products.map((item: any) => ({
-      productId: item.id.toString(),
-    }));
-  } catch (error) {
-    console.error("Error fetching product ids:", error);
-    return [];
-  }
-}
 
 const Page = () => {
+
+  
   return (
     <div className="card">
       <PageClient />
